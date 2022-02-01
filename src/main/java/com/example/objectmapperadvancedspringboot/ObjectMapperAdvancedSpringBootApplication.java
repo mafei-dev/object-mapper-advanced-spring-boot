cc73724a5861eeaf6e276fbd32312d1d96a98957
mafei <kalhara.me@gmail,com>
2022-02-01 21:52:44 +0530

    Initial commit

---
+++ src/main/java/com/example/objectmapperadvancedspringboot/ObjectMapperAdvancedSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.objectmapperadvancedspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectMapperAdvancedSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectMapperAdvancedSpringBootApplication.class, args);
+    }
+
+}
